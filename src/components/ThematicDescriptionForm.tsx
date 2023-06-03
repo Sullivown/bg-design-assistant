05f4d513c4d12d0bc0a55ced73e0bd170b287f56
Sullivown <daniel.w.t.sullivan@gmail.com>
2023-06-03 20:17:15 +0100

    Complete ThematicForm

--- src/components/ThematicDescriptionForm.tsx
+++ src/components/ThematicDescriptionForm.tsx
@@ -1,13 +1,20 @@
+import Button from '@/elements/Button';
 import React from 'react';
 
 export default function ThematicDescriptionForm() {
 	return (
 		<form>
-			<div className='space-y-12'>
-				<h2 className='text-base font-semibold leading-7 text-gray-900'>
+			<div className='space-y-8'>
+				<h2 className='text-base text-center font-semibold leading-7 text-gray-900'>
 					Instructions
 				</h2>
-				<p className='mt-1 text-sm leading-6 text-gray-600'>
+				<p className='mt-1 leading-6 text-center'>
+					Input your idea for a board game theme and let me help you
+					suggest mechanisms to match! Give as much or as little
+					detail as you want; a few sentences should be enough for me
+					to go on, but feel free to experiment.
+				</p>
+				<p className='mt-1 text-sm leading-6 text-center'>
 					Please note that this information will be shared with
 					OpenAI, so please do not include any personal or sensitive
 					information.
@@ -15,19 +22,22 @@ export default function ThematicDescriptionForm() {
 				<div className='col-span-full'>
 					<label
 						htmlFor='theme-description'
-						className='block text-sm font-medium leading-6 text-gray-900'
+						className='block font-medium leading-6 text-gray-900'
 					>
-						Theme
+						Describe your thematic idea:
 					</label>
 					<div className='mt-2'>
 						<textarea
 							id='theme-description'
 							name='theme-description'
-							rows={3}
-							className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+							rows={5}
+							className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-pink placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-dark sm:text-sm sm:leading-6'
 							defaultValue={''}
 						/>
 					</div>
+				</div>
+				<div className='flex flex-row justify-end'>
+					<Button>Submit</Button>
 				</div>
 			</div>
 		</form>
