a2a932589150d5507acc21788c30b4287272ae5d
Sullivown <daniel.w.t.sullivan@gmail.com>
2023-06-03 19:50:23 +0100

    Update footer imports

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-import { WrenchScrewdriverIcon } from '@heroicons/react/24/solid';
-
 export default function Footer() {
 	return (
 		<footer className='flex bg-dark text-white py-6 text-sm justify-center space-x-2 mt-auto'>
