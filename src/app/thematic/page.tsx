6ea171cd3cc40eef758eaa836dec84aa8a91785b
Sullivown <daniel.w.t.sullivan@gmail.com>
2023-06-03 19:16:57 +0100

    Custom colours issue

--- src/app/thematic/page.tsx
+++ src/app/thematic/page.tsx
@@ -1,9 +1,13 @@
 import Section from '@/elements/Section';
+import GameDetails from '@/components/GameDetails';
 
 export default function ThematicPage() {
 	return (
-		<Section className='text-center'>
-			<h2>Coming soon!</h2>
-		</Section>
+		<>
+			<Section className='text-center text-2xl font-semibold mb-5'>
+				<h2>Generate ideas from a thematic description</h2>
+			</Section>
+			<GameDetails />
+		</>
 	);
 }
