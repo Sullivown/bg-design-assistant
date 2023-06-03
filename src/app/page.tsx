72f86dba6741097d7f318fb40378718d6ae5fd89
Sullivown <daniel.w.t.sullivan@gmail.com>
2023-06-03 16:19:14 +0100

    Add generation routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
 import Button from '@/elements/Button';
 import Section from '@/elements/Section';
+import Link from 'next/link';
 
-export default function Home() {
+export default function HomePage() {
 	return (
 		<Section className='space-y-10'>
 			<h2 className='text-center font-semibold text-2xl'>
 				Welcome to Board Game Design Assistant!
 			</h2>
-			<p>Some intro text</p>
+			<div className='text-center'>
+				<p>Hello!</p>
+				<p>
+					Welcome to the Board Game Design Assistant! an OpenAI
+					powered tool for assisting with the board game design
+					process.
+				</p>
+			</div>
+
 			<div>
 				<h3>Select your method:</h3>
 				<div className='flex flex-row flex-wrap sm:flex-nowrap justify-center gap-x-10 gap-y-5 my-5'>
-					<Button className='w-full sm:w-1/3'>Random</Button>
-					<Button className='w-full sm:w-1/3'>
-						Thematic Description
-					</Button>
-					<Button className='w-full sm:w-1/3 '>Mechanisms</Button>
+					<Link href='/random' className='w-full sm:w-1/3'>
+						<Button className='w-full'>Random</Button>
+					</Link>
+					<Link href='/thematic' className='w-full sm:w-1/3'>
+						<Button className='w-full'>Thematic Description</Button>
+					</Link>
+					<Link href='/mechanisms' className='w-full sm:w-1/3'>
+						<Button className='w-full'>Mechanisms</Button>
+					</Link>
 				</div>
 			</div>
 		</Section>
---
+++ src/app/thematic/page.tsx
@@ -0,0 +1,9 @@
+import Section from '@/elements/Section';
+
+export default function ThematicPage() {
+	return (
+		<Section className='text-center'>
+			<h2>Coming soon!</h2>
+		</Section>
+	);
+}
