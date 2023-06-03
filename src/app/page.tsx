75e5372723c29f82887a388331dddb32d5e6e1b1
Sullivown <daniel.w.t.sullivan@gmail.com>
2023-06-03 14:04:03 +0100

    Add elements folder, Button element and update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Button from '@/elements/Button';
+
 export default function Home() {
 	return (
 		<div className='flex flex-col justify-center content-center bg-white w-full max-w-7xl p-4 sm:p-6 lg:p-8 rounded-lg'>
@@ -8,15 +10,9 @@ export default function Home() {
 			<div>
 				<h3>Select your method:</h3>
 				<div className='flex flex-row justify-center gap-x-10'>
-					<button className='px-4 py-5 w-1/3 rounded-lg bg-pink hover:bg-pink-dark text-white text-xl'>
-						Random
-					</button>
-					<button className='px-4 py-5 w-1/3 rounded-lg bg-pink hover:bg-pink-dark text-white text-xl'>
-						Thematic Description
-					</button>
-					<button className='px-4 py-5 w-1/3 rounded-lg bg-pink hover:bg-pink-dark text-white text-xl'>
-						Mechanisms
-					</button>
+					<Button className='w-1/3'>Random</Button>
+					<Button className='w-1/3'>Thematic Description</Button>
+					<Button className='w-1/3 '>Mechanisms</Button>
 				</div>
 			</div>
 		</div>
---
+++ src/elements/Button.tsx
@@ -0,0 +1,16 @@
+import { HTMLAttributes, ReactNode } from 'react';
+
+type ButtonProps = {
+	children: ReactNode;
+	className: string;
+};
+
+export default function Button({ children, className }: ButtonProps) {
+	return (
+		<button
+			className={`px-4 py-5 rounded-lg bg-pink hover:bg-pink-dark text-white text-xl ${className}`}
+		>
+			{children}
+		</button>
+	);
+}
